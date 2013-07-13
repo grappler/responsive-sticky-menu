105c22d34a8f3a6d82b328c77297924e57492913
Ulrich Pogson <grapplerulrich@gmail.com>
2013-07-13 09:26:39 +0200

    Fix issue with multiple mobile menus

--- js/sticky-menu.js
+++ js/sticky-menu.js
@@ -2,9 +2,9 @@ jQuery(function( $ ){
 	$(window).scroll(function() {
 		var yPos = ( $(window).scrollTop() );
 		if(yPos > 100) { // show sticky menu after screen has scrolled down 200px from the top
-			$("#sticky-menu").fadeIn();
+			document.getElementById('sticky-menu').style.left = '0';
 		} else {
-			$("#sticky-menu").fadeOut();
+			document.getElementById('sticky-menu').style.left = '';
 		}
 	});
 });
